ca83806e440cca132153ff549df834c5e43409ba
Bartłomiej Klocek <barthap10@gmail.com>
2021-07-08 12:46:16 +0200

    Add jest

--- App.tsx
+++ App.tsx
@@ -1,21 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// This file is an entry point for Expo. We just delegate to `/src/App.tsx`
+import App from "./src/App";
+export default App;
